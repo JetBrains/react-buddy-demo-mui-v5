5c443e565cd8403ab0112f37c76030fb040c9a76
Vlad Minaev <minaev@haulmont.com>
2021-09-14 17:40:38 +0400

    Revert handson

--- react-buddy-app/src/App.tsx
+++ react-buddy-app/src/App.tsx
@@ -2,29 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-export class NewComponent extends React.Component {
-  render() {
-    return <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo"/>
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-
-    </header>;
-  }
-}
-
 function App() {
   return (
     <div className="App">
-      <a
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- react-buddy-app/src/dev/previews.tsx
+++ react-buddy-app/src/dev/previews.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Previews, ComponentPreview } from '@react-buddy/ide-toolbox';
-import App, {NewComponent} from '../App';
+import App from '../App';
 
 export const ComponentPreviews = () => {
   return (
@@ -8,9 +8,6 @@ export const ComponentPreviews = () => {
       <ComponentPreview path="/App">
         <App />
       </ComponentPreview>
-        <ComponentPreview path="/NewComponent">
-            <NewComponent/>
-        </ComponentPreview>
     </Previews>
   );
 };
