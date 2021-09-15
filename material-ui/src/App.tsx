ad80e8b45b941cf539e230056ce1ed277fde4b2b
Vlad Minaev <minaev@haulmont.com>
2021-09-15 15:53:51 +0400

    MUI Palette

--- material-ui/src/App.tsx
+++ material-ui/src/App.tsx
@@ -1,32 +1,10 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
-
-export function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
+import {Box, Button, Container, Stack} from "@mui/material";
 
 export default function App() {
   return (
     <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v5-beta example with TypeScript
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
+
     </Container>
   );
 }
--- material-ui/src/dev/palette.tsx
+++ material-ui/src/dev/palette.tsx
@@ -5,13 +5,135 @@ import {
   Variant,
   Palette,
 } from '@react-buddy/ide-toolbox';
+import {
+  Autocomplete,
+  Box,
+  Button,
+  Container,
+  Divider, Grid,
+  IconButton,
+  ImageList,
+  ImageListItem,
+  Stack,
+  TextField
+} from '@mui/material';
+import {Add, Delete, Edit} from '@mui/icons-material';
 
 export default () => (
   <Palette>
-    <Category name="Text">
-      <Component name="Hello meessage">
+    <Category name="Layout">
+      <Component name="Box">
         <Variant>
-          <span> Hello, Create React Buddy App! </span>
+          <Box></Box>
+        </Variant>
+      </Component>
+      <Component name="Container">
+        <Variant>
+          <Container maxWidth="sm"></Container>
+        </Variant>
+        <Variant name="fixed">
+          <Container fixed></Container>
+        </Variant>
+      </Component>
+      <Component name="Stack">
+        <Variant>
+          <Stack my={1} spacing={2}>
+            <Box sx={{ border: '1px dashed grey' }}>Item1</Box>
+            <Box sx={{ border: '1px dashed grey' }}>Item2</Box>
+            <Box sx={{ border: '1px dashed grey' }}>Item3</Box>
+          </Stack>
+        </Variant>
+        <Variant name="horizontal">
+          <Stack
+              my={1}
+              direction="row"
+              divider={<Divider orientation="vertical" flexItem />}
+              spacing={2}
+          >
+            <Box sx={{ border: '1px dashed grey' }}>Item 1</Box>
+            <Box sx={{ border: '1px dashed grey' }}>Item 2</Box>
+            <Box sx={{ border: '1px dashed grey' }}>Item 3</Box>
+          </Stack>
+        </Variant>
+      </Component>
+      <Component name="Grid">
+        <Variant>
+          <Grid container spacing={2}>
+            <Grid item xs={6} md={8}>
+              <Box>xs=6 md=8</Box>
+            </Grid>
+            <Grid item xs={6} md={4}>
+              <Box>xs=6 md=4</Box>
+            </Grid>
+            <Grid item xs={6} md={4}>
+              <Box>xs=6 md=4</Box>
+            </Grid>
+            <Grid item xs={6} md={8}>
+              <Box>xs=6 md=8</Box>
+            </Grid>
+          </Grid>
+        </Variant>
+      </Component>
+      <Component name="ImageList">
+        <Variant>
+          <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+            <ImageListItem>
+              <img
+                  src={`https://images.unsplash.com/photo-1551963831-b3b1ca40c98e`}
+                  srcSet={`https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                  alt={"Breakfast"}
+                  loading="lazy"
+              />
+            </ImageListItem>
+            <ImageListItem>
+              <img
+                  src={`https://images.unsplash.com/photo-1522770179533-24471fcdba45`}
+                  srcSet={`https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                  alt={"Camera"}
+                  loading="lazy"
+              />
+            </ImageListItem>
+          </ImageList>
+        </Variant>
+      </Component>
+    </Category>
+    <Category name="Inputs">
+      <Component name="Autocomplete">
+        <Variant>
+          <Autocomplete
+              disablePortal
+              options={[]}
+              sx={{ width: 300 }}
+              renderInput={(params) => <TextField {...params} label="Movie" />}
+          />
+        </Variant>
+      </Component>
+      <Component name="Button">
+        <Variant requiredParams={["variant"]}>
+          <Button>Text</Button>
+        </Variant>
+        <Variant name="contained">
+          <Button variant="contained">Contained</Button>
+        </Variant>
+        <Variant name="outlined">
+          <Button variant="outlined">Outlined</Button>
+        </Variant>
+      </Component>
+      <Component name="IconButton">
+        <Variant name="add">
+          <IconButton aria-label="add">
+            <Add/>
+          </IconButton>
+        </Variant>
+        <Variant name="edit">
+          <IconButton aria-label="edit">
+            <Edit/>
+          </IconButton>
+        </Variant>
+        <Variant name="delete">
+          <IconButton aria-label="delete">
+            <Delete/>
+          </IconButton>
         </Variant>
       </Component>
     </Category>
