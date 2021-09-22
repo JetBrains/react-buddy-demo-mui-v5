03ff2d1f897c540905bcf68b0c0b209726c0203e
Vlad Minaev <minaev@haulmont.com>
2021-09-22 22:20:01 +0400

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
 import * as React from 'react';
-import {
-  BottomNavigation,
-  BottomNavigationAction,
-  Breadcrumbs,
-  Button,
-  Container, Drawer,
-  IconButton,
-  Link, List, ListItem, ListItemText, Typography
-} from "@mui/material";
-import {Add, Favorite, LocationOn, Restore} from "@mui/icons-material";
+import {Container} from "@mui/material";
 
 export default function App() {
   return (
--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Previews, ComponentPreview } from '@react-buddy/ide-toolbox';
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox';
 import App from '../App';
-import {ConditionalComponent3, JsxTreeDemoScreen} from "../OutlineDemo";
-import {FuncExprComponent} from "../PreviewDemo";
 
 export const ComponentPreviews = () => {
   return (
@@ -10,15 +8,6 @@ export const ComponentPreviews = () => {
       <ComponentPreview path="/App">
         <App />
       </ComponentPreview>
-        <ComponentPreview path="/JsxTreeDemoScreen">
-            <JsxTreeDemoScreen/>
-        </ComponentPreview>
-        <ComponentPreview path="/ConditionalComponent3">
-            <ConditionalComponent3/>
-        </ComponentPreview>
-        <ComponentPreview path="/FuncExprComponent">
-            <FuncExprComponent title={'Title'}/>
-        </ComponentPreview>
     </Previews>
   );
 };
