cbe59fcc64e34dc8d76175aac7a2a20b23fb62c0
Vlad Minaev <minaev@haulmont.com>
2021-09-22 22:18:47 +0400

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import {Add, Favorite, LocationOn, Restore} from "@mui/icons-material";
 export default function App() {
   return (
     <Container maxWidth="sm">
-      <Link href="#">Link</Link>
     </Container>
   );
 }
