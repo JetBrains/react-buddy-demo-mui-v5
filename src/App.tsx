2de744765b72091cbd74c6328727f55dfff581cf
Vlad Minaev <minaev@haulmont.com>
2021-09-30 16:02:16 +0400

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
 import * as React from 'react';
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Alert,
-  AppBar, Button, Card, CardActions, CardContent,
-  Container, IconButton, Paper, SpeedDial, SpeedDialAction, SpeedDialIcon,
-  Stack, Toolbar,
-  Typography
-} from "@mui/material";
-import {ExpandMore, Menu, Print, Save, Share} from "@mui/icons-material";
+import {Container} from "@mui/material";
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-
-    </Container>
+    <Container maxWidth="sm"></Container>
   );
 }
