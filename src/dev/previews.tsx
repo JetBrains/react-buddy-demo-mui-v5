f093996dfe4c50cc98defd24c8731bbb6693fa97
VorobevAndrey <vorobevaa@haulmont.com>
2022-01-24 16:29:41 +0400

    palette preview
    (cherry picked from commit 805bbcd243e2df24323434ab6616c5f71e4c9123)

--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox';
 import App from '../App';
+import PaletteTree from './palette';
 
 export const ComponentPreviews = () => {
   return (
-    <Previews>
+    <Previews palette={<PaletteTree/>}>
       <ComponentPreview path="/App">
         <App />
       </ComponentPreview>
