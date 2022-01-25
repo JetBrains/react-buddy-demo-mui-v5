7657aaa2e1ca6b9adc083f4a46b63af1479e5be3
VorobevAndrey <vorobevaa@haulmont.com>
2022-01-25 14:25:12 +0400

    Revert "palette preview"
    This reverts commit 805bbcd243e2df24323434ab6616c5f71e4c9123.

--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox';
 import App from '../App';
-import PaletteTree from './palette';
 
 export const ComponentPreviews = () => {
   return (
-    <Previews palette={<PaletteTree/>}>
+    <Previews>
       <ComponentPreview path="/App">
         <App />
       </ComponentPreview>
