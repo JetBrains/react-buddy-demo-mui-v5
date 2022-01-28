0be1299d9a2a6d2f42e1b991d8cf8ae2339ebb75
Vlad Minaev <minaev@haulmont.com>
2022-01-28 20:07:39 +0400

    Component preview

--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox';
 import App from '../App';
+import ProjectPalette from './palette'
 
 export const ComponentPreviews = () => {
   return (
-    <Previews>
+    <Previews palette={<ProjectPalette/>}>
       <ComponentPreview path="/App">
         <App />
       </ComponentPreview>
