af1f0d0fc044f3e2ea06a7be1f29234ef7c9ddee
Vlad Minaev <minaev@haulmont.com>
2022-03-21 12:58:04 +0400

    Add JSX example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
 import * as React from 'react';
-import {Container} from "@mui/material";
+import {Button, Card, CardActions, CardContent, CircularProgress, Container, Typography} from "@mui/material";
+import {MouseEvent, useEffect, useState} from "react";
 
 export default function App() {
+
+
+  const [loading, setLoading] = useState(0);
+  const [items, setItems] = useState<number[]>([]);
+
+  useEffect(() => {
+    setItems([1,2,3])
+  });
+
+  function onButtonClick(event: MouseEvent<HTMLButtonElement>) {
+    // todo
+  }
+
+  if (loading) {
+    return <CircularProgress/>
+  }
+
   return (
-    <Container maxWidth="sm"></Container>
+    <Container maxWidth="sm">
+      {items.map(item => (
+        <Card variant="outlined">
+          <CardContent>
+            <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+              Word of the Day
+            </Typography>
+            <Typography variant="body2">
+              well meaning and kindly.
+              <br/>
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small"
+                    onClick={onButtonClick}
+                    onMouseDown={() => {}}>
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+      ))}
+    </Container>
   );
 }
